9eefe66004fd098c3998f2240fe258c4ad6da3eb
ajju2003 <87330000+ajju2003@users.noreply.github.com>
2022-08-06 12:41:35 +0530

    Delete digit_count.py

--- digit_count.py
+++
@@ -1,8 +0,0 @@
-def numberof_words(string):
-    count = 0
-    for i in string:
-        if i == ' ':
-            count = count+1
-    return count
-a = numberof_words('I AM A GOOD BOY')
-print(a)
